d8f20ff402ffb1f5aa7dbbedafbd4b18c7ce4250
Opak Alex <opak.alexandr@gmail.com>
2019-07-09 10:10:21 +0200

    fixes

--- setup.sh
+++ setup.sh
@@ -5,14 +5,3 @@ git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 mkdir -p ~/.vim/colors
 ln -fs $DIR/vimrc ~/.vimrc
 ln -fs $DIR/custom.vim ~/.vim/colors/custom.vim
-
-# ZSH
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-ln -fs $DIR/zshrc ~/.zshrc
-ln -fs $DIR/zshenv ~/.zshenv
-ln -fs $DIR/hl7.zsh-theme ~/.oh-my-zsh/themes/hl7.zsh-theme
-
-# OTHER
-ln -fs $DIR/gemrc ~/.gemrc
-ln -fs $DIR/gitconfig ~/.gitconfig
-ln -fs $DIR/rspec ~/.rspec
